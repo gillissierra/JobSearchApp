5b9e31670985e7cb59ab2acbc98c17d1fe7adbf3
gillissierra <gillissierra@gmail.com>
2018-03-10 17:46:15 -0500

    03.09.18 Basic checkboxes

--- app/src/main/java/com/example/user/jobsearchapp/MainActivity.java
+++ app/src/main/java/com/example/user/jobsearchapp/MainActivity.java
@@ -5,9 +5,48 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void onCheckboxClicked(android.view.View view) {
+        //Array to store filters
+
+        //Checking if it is checked
+        boolean checked = ((android.widget.CheckBox) view).isChecked();
+
+        // Check which checkbox is clicked
+        //Education
+        switch(view.getId()) {
+            case R.id.checkBoxPhD:
+                if (checked)
+                    //Send PhD into query terms
+
+                    break;
+            case R.id.checkBoxMSc:
+                if (checked)
+                    //Send Masters into query terms
+                    break;
+            case R.id.checkBoxBSc:
+                if (checked)
+                    //Send Bachelor's into query terms
+                    break;
+        }
+        switch(view.getId()) {
+            case R.id.checkBoxSenior:
+                if (checked)
+                    //Send Senior into query terms
+                    break;
+            case R.id.checkBoxAssoc:
+                if (checked)
+                    //Send Assistant into query terms
+                    break;
+        }
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Grab the website source
 }
